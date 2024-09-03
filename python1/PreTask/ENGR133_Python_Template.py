41dccd55d24b035dcf1792f1b292d194feacc591
This-is-leeo <135179953+This-is-leeo@users.noreply.github.com>
2024-09-03 11:51:59 -0400

    Added basic folders

---
+++ python1/PreTask/ENGR133_Python_Template.py
@@ -0,0 +1,46 @@
+"""
+Course Number: ENGR 13300
+Semester: e.g. Fall 2024
+
+Description:
+    Replace this line with a description of your program.
+
+Assignment Information:
+    Assignment:  pretask 1
+    Team ID:     team 03
+    Author:      yu1398@purdue.edu
+    Date:        09/03/2024
+
+Contributors:
+    Name, login@purdue [repeat for each]
+
+    My contributor(s) helped me:
+    [ ] understand the assignment expectations without
+        telling me how they will approach it.
+    [ ] understand different ways to think about a solution
+        without helping me plan my solution.
+    [ ] think through the meaning of a specific error or
+        bug present in my code without looking at my code.
+    Note that if you helped somebody else with their code, you
+    have to list that person as a contributor here as well.
+
+Academic Integrity Statement:
+    I have not used source code obtained from any unauthorized
+    source, either modified or unmodified; nor have I provided
+    another student access to my code.  The project I am
+    submitting is my own original work.
+"""
+
+""" Write any import statements here (and delete this line)."""
+
+import math
+def main():
+    a = 5
+    b = 9 
+    c = 2
+    
+    
+
+
+if __name__ == "__main__":
+    main()
